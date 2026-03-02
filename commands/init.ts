8a51076dcd6ac80858a89424c40a79564d4dde96
Subham Sangwan <subhamsangwan26@gmail.com>
2026-03-02 11:50:45 +0530

    feat: auto-generate tsconfig and index.ts in init command

--- commands/init.ts
+++ commands/init.ts
@@ -13,6 +13,8 @@ IntializesTS(foldername){
     this.run(`mkdir ${foldername}`)
     this.run(`cd ${foldername} && npm init -y`)
     this.run(`cd ${foldername} && npm install typescript @types/node --save-dev`)
+    this.run(`cd ${foldername} && npx tsc --init`)
+    this.run(`cd ${foldername} && touch index.ts`)
 }
 run(command) {
     execSync(command,{stdio:"inherit"})
